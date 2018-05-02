64e8848b4773ee28214777d11d3049be845eaaba
wim <wim.bervoets@gmail.com>
2018-05-02 20:28:56 +0200

    update

--- app/src/main/java/be/jatra/simplecalculator/app/MainActivity.kt
+++ app/src/main/java/be/jatra/simplecalculator/app/MainActivity.kt
@@ -3,8 +3,8 @@ package be.jatra.simplecalculator.app
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 import be.jatra.simplecalculator.IntentConstants
